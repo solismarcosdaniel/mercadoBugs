fc2f74747c2a0dcb560e327e7a254ce97dcbe169
marcos daniel <solismarcosdaniel@gmail.com>
2021-10-13 18:31:28 -0300

    nuevo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express ();
 const path = require('path');
 
 
+
 const publicPath = path.resolve(__dirname,"./public")
 app.use(express.static(publicPath))
 app.get("/",(req,res)=> {res.sendFile(path.resolve(__dirname,"./views/home.html"))});
